89a4beaa16591ba6d3ab87f855a198f5316c512c
Blancaslan <d3nnisc3k3r@gmail.com>
2022-09-29 01:46:33 +0100

    Setting up project and working on button
    This is the first commit for the stopwatch repo, this commit has the initial project made and has some of button design done. Button functionality is still WIP.

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CustomButton from './components/button/button';
+
+function App() {
+  return (
+    <div>
+      <CustomButton onclick="Hello my ladies"></CustomButton>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/button/button.js
@@ -0,0 +1,9 @@
+import './button.css'
+
+export default function CustomButton() {
+    return (
+        <button className="outerbox" onclick="startstop">
+            <h1 className="startstop">Start/Stop</h1>
+        </button>
+    )
+}

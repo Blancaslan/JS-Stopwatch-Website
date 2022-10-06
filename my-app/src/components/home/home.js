89684805e38c2845bf4492341efca82d6cc4a785
Blancaslan <68610191+Blancaslan@users.noreply.github.com>
2022-10-06 11:00:48 +0100

    Adding miliseconds and resizing the backbox

--- my-app/src/components/home/home.js
+++ my-app/src/components/home/home.js
@@ -3,11 +3,14 @@ import CustomButton from '../button/button';
 import React  from 'react';
 
 
-let second = 0
+let second = 3500
 let timer
 let Minute = '00'
 let Second = '00'
-let intervalID
+let MiliSecond = "00"
+let miliTimer
+let secondIntervalID
+
 
 export default function Home() {
     return(
@@ -15,6 +18,7 @@ export default function Home() {
             <div className="backbox">
                 <div className="time">
                     <h1 id="count">{Minute}:{Second}</h1>
+                    <p id="smallCount">{MiliSecond}</p>
                 </div>
                 <div className="buttons">
                     <CustomButton text={"Stop"} onClick={stop} backgroundColor="red"></CustomButton>
@@ -27,39 +31,56 @@ export default function Home() {
 }
 
 function start() {
-    if (!intervalID) {
-        intervalID = setInterval(() => {
-            second++
-            timer = Time(second)
-            Minute = timer[0]
-            Second = timer[1]
-            document.getElementById("count").innerHTML=[Minute + ':' + Second]
-        }, 1000)
+    if (!secondIntervalID) {
+        secondIntervalID = setInterval(() => {
+            if (miliTimer < 99){
+                miliTimer++
+                MiliSecond = miliSecSwitch(miliTimer)
+                document.getElementById("smallCount").innerHTML=MiliSecond
+            }
+            else {
+                miliTimer = 0
+                second++
+                timer = Time(second)
+                document.getElementById("count").innerHTML=[timer[0] + ':' + timer[1]]
+            }
+        }, 10)
     }
+
+
     else {
         stop()
     }
 }
-
 function stop() {
-    clearInterval(intervalID)
-    intervalID = undefined
+    clearInterval(secondIntervalID)
+    secondIntervalID = undefined
 }
 
 function reset() {
     second = 0
+    MiliSecond = '00'
     Second = '00'
     Minute = '00'
     document.getElementById("count").innerHTML=[Minute + ':' + Second]
-    clearInterval(intervalID)
-    intervalID = undefined
+    document.getElementById("smallCount").innerHTML=MiliSecond
+    clearInterval(secondIntervalID)
+    secondIntervalID = undefined
 }
 
-function secMinSwitch(sec, min) {
+function miliSecSwitch(miliTime) {
+    switch (true) {
+        case miliTime === 0: {miliTime = '00'; break}    
+        case miliTime < 10: {miliTime = '0' + miliTime; break} 
+    }
+    return miliTime
+}
+
+function secMinSwitch(sec, ...min) {
     switch (true) {
         case sec === 0: {sec = '00'; break}    
         case sec < 10: {sec = '0' + sec; break} 
-    }  
+    }
     switch (true) {  
         case min === 0: {min = '00'; break}
         case min < 10: {min = '0' + min; break}
